5e76e2b844fb9a490f9239687eae4b9e9bf57f35
Alireza <64541682+Artaherzadeh@users.noreply.github.com>
2026-07-17 21:31:19 +0330

    sync: update app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.alireza.podcasts"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.3.4"
+        versionCode = 8
+        versionName = "1.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
