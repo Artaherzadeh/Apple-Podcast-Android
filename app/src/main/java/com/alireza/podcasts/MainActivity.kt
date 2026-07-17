4ca852b2f78a3ca2dff0312f0fa974c37a655c12
Alireza <64541682+Artaherzadeh@users.noreply.github.com>
2026-07-17 21:17:04 +0330

    sync: update app/src/main/java/com/alireza/podcasts/MainActivity.kt

--- app/src/main/java/com/alireza/podcasts/MainActivity.kt
+++ app/src/main/java/com/alireza/podcasts/MainActivity.kt
@@ -710,15 +710,6 @@ class MainActivity : AppCompatActivity() {
                             textSpan.innerText = 'Download Episode';
                         }
                         
-                        const iconContainer = button.querySelector('.contextual-menu-item__icon-container');
-                        if (iconContainer) {
-                            iconContainer.innerHTML = `
-                                <svg viewBox="0 0 24 24" fill="none" stroke="#E5E5EA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="width: 16px; height: 16px; margin-top: 4px;">
-                                    <path d="M21 15V16.2C21 17.8802 21 18.7202 20.673 19.362C20.3854 19.9265 19.9265 20.3854 19.362 20.673C18.7202 21 17.8802 21 16.2 21H7.8C6.11984 21 5.27976 21 4.63803 20.673C4.07354 20.3854 3.6146 19.9265 3.32698 19.362C3 18.7202 3 17.8802 3 16.2V15M17 10L12 15M12 15L7 10M12 15V3"></path>
-                                </svg>
-                            `;
-                        }
-                        
                         button.onclick = function(e) {
                             e.stopPropagation();
                             e.preventDefault();
